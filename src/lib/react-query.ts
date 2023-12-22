d6da54f8ee68c1c3ec75b611993211dc57141647
Adnan Radwan <adnanradwan_8@hotmail.com>
2023-12-21 23:31:12 -0500

    resumePaused

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,14 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { removeOldestQuery } from "@tanstack/react-query-persist-client";
-import { compress, decompress } from "lz-string";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       retry: 0,
-      staleTime: 5 * 60 * 100,
+      staleTime: 5 * 60 * 1000,
       gcTime: Infinity,
     },
   },
@@ -16,9 +15,6 @@ const queryClient = new QueryClient({
 
 const localStoragePersister = createSyncStoragePersister({
   key: "paperkitchen_cache",
-  serialize: (data) => compress(JSON.stringify(data)),
-  deserialize: (data) => JSON.parse(decompress(data)),
-  retry: removeOldestQuery,
   storage: window.localStorage,
 });
 
